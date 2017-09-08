8911698b1a8d96cc34629ecdd7626e43e8b22495
Connor <clynch1@samford.edu>
2017-09-07 23:12:30 -0500

    Created two new pages and then linked one of them to the login button.

--- app/src/main/java/mixr/mixr/WelcomeScreen.java
+++ app/src/main/java/mixr/mixr/WelcomeScreen.java
@@ -1,18 +1,54 @@
 package mixr.mixr;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 public class WelcomeScreen extends AppCompatActivity {
 
+    Button GoToNewActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
-    }
+        GoToNewActivity = (Button)findViewById(R.id.button1);
 
+        GoToNewActivity.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                // Intent code for open new activity through intent.
+
+                Intent intent = new Intent(WelcomeScreen.this, HomeScreen.class);
+                startActivity(intent);
+
+            }
+        });
+    }
 }
+//package mixr.mixr;
+//
+//        import android.app.Activity;
+//        import android.content.Intent;
+//        import android.support.v7.app.AppCompatActivity;
+//        import android.os.Bundle;
+//        import android.view.Menu;
+//        import android.view.MenuItem;
+//        import android.view.View;
+//        import android.widget.Button;
+//
+//public class WelcomeScreen extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_welcome_screen);
+//    }
+//
+//}
