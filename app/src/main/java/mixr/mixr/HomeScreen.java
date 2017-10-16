656e15dc1a3588ba6c5ea15af14bb81ca6e1a923
Connor <clynch1@samford.edu>
2017-10-15 22:09:13 -0500

    Created top navigation, fragments for each main page, and started to add contents to the fragments.  The layout needs to tweaked more but it is just a place holder.

--- app/src/main/java/mixr/mixr/HomeScreen.java
+++ app/src/main/java/mixr/mixr/HomeScreen.java
@@ -1,6 +1,8 @@
 package mixr.mixr;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -11,22 +13,24 @@ import android.widget.TextView;
 
 public class HomeScreen extends AppCompatActivity {
 
-    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+            android.support.v4.app.FragmentTransaction transaction = fragmentManager.beginTransaction();
+
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_account:
+                    transaction.replace(R.id.content,new AccountFragment()).commit();
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_explore);
+                case R.id.navigation_explore:
+                    transaction.replace(R.id.content,new ExploreFragment()).commit();
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_matches:
+                    transaction.replace(R.id.content,new MatchesFragment()).commit();
                     return true;
             }
             return false;
@@ -39,9 +43,12 @@ public class HomeScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+        android.support.v4.app.FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.content,new AccountFragment()).commit();
     }
 }
 
